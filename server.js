34e751f2b38a6276e83fe4446dabbbe4e98fa6b9
MKS-students <mks.students@hackreactor.com>
2015-07-31 16:54:15 -0700

    build

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require('./server-config.js');
 
-var port = 4568;
+app.set('port', (process.env.PORT || 4568));
 
-app.listen(port);
+app.listen(app.get('port'));
 
-console.log('Server now listening on port ' + port);
+console.log('Server now listening on port',app.get('port'));
